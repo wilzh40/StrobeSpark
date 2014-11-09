c302db462c4ca43e7fe9d3d320385bdaf6ce5e75
wilzh40 <wilzh40@gmail.com>
2014-11-09 04:44:39 -0800

    changed sc stuff"

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-
+ToneDenReady = window.ToneDenReady || [];
+ToneDenReady.push(function() {
+    // This is where all the action happens:
+    ToneDen.player.create({
+        shrink: false,
+        single: true,
+        dom: "#player",
+        eq: "waves",
+        skin: "light",
+        tracksPerArtist: 4,
+        urls: [
+            "https://soundcloud.com/lil_jon/turn-down-for-what-dj-snake"
+        ]
+    });
+});
